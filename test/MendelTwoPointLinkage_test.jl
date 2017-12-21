c2e184fc257e226ef2714c1a77e785c211b70cd0
benjamin chu <benchu99@hotmail.com>
2017-12-20 20:53:49 -0800

    begin unit testing

---
+++ test/MendelTwoPointLinkage_test.jl
@@ -0,0 +1,25 @@
+using MendelTwoPointLinkage
+using MendelBase
+using Search
+using SearchSetup
+using DataFrames  
+
+@testset "wrapper and basics" begin
+
+end
+
+@testset "penetrance function" begin
+    
+end
+
+@testset "prior function" begin
+    
+end
+
+@testset "transmission function" begin
+    
+end
+
+@testset "initialize_optimization_two_point_linkage" begin
+    
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,7 @@ using MendelTwoPointLinkage
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+include("MendelTwoPointLinkage_test.jl")
+
+# julia -e 'Pkg.test("MendelTwoPointLinkage",coverage=true)'
+# @show get_summary(process_file("src/MendelTwoPointLinkage.jl"))
